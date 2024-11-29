017094d9e51a2e8237f8735d2f100da019bd5cf3
Abdulla <kaituevabdulla@yandex.ru>
2024-11-29 22:32:09 +0300

    update

--- main.cpp
+++ main.cpp
@@ -42,19 +42,23 @@ void PrintData(const std::unordered_map<int, config::RowType> &data, const std::
 
 int main() {
     memdb::Database &db = memdb::Database::getInstance();
-
-    std::string create_table_query = "create tABle family ({unique} id : int32, name : string[32], isParent : bool, code : bytes[3])";
-
+    std::string create_table_query = "create table medicine ({key, autoincrement} id : int32, doctors : string[32], equipment : bool, age : int32)";
     auto res = db.execute(create_table_query);
 
-    auto res1 = db.execute(R"(insert (id = 1, name = "Anna", isParent = false, code = 0x0ffee3) to family)");
-    auto res2 = db.execute(R"(insert (id = 2, name = "Ivan", isParent = true, code = 0x0e572f) to family)");
+    std::string create_index_query = "create ordered index on medicine by id";
+    auto res1 = db.execute(create_index_query);
 
-    std::string delete_query = R"(DELETE family where code = 0x0ffee3)";
-    auto res3 = db.execute(delete_query);
+    for (int i = 0; i <= 10; ++i) {
+        std::string insert_query = R"(insert (doctors = "Strange)" + std::to_string(i) + R"(", age =)" + std::to_string(i) + R"() to medicine)";
+        auto res2 = db.execute(insert_query);
+    }
 
-    auto table = db.getTable("family");
-    auto data_after = table->getData();
+    PrintData(db.getTable("medicine")->getData(), "all Table");
 
-    PrintData(data_after, delete_query);
+    std::unique_ptr<memdb::Response> res5;
+    std::string select_query = R"(update medicine set doctors = doctors + "1" where ( ( id % 2 = 1 && id > 200 ) || ( doctors <= "Strange5" ) ) && ( id = age ) && ( id < 40 ) && id >= 9)";
+    res5 = db.execute(select_query);
+    auto data = res5->getData();
+    std::cout << res5->getMessage();
+    PrintData(db.getTable("medicine")->getData(), select_query);
 }
